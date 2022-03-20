365857aad565f389d3d07a4bafb7bc259c2b263d
Anand Kusuma langgeng <anandkusuma27@gmail.com>
2022-03-20 22:37:51 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WP2_Mas-Anand
---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>Perkenalan</h1><br>";
+        echo "Nama saya Anand Kusuma Langgeng
+        Saya tinggal di daerah Depok
+        olah raga yang saya sukai adalah berenang";
+	}
+
+}
